3b7f4ee98e971b2ae5b5d0a0be7b52b88b7568de
Lucas Pérez <lperezparracho@danielcastelao.org>
2023-03-06 14:49:52 +0100

    I2

--- src/implementacionesColaborador.java
+++ src/implementacionesColaborador.java
@@ -1,5 +1,6 @@
 public class implementacionesColaborador {
     public static void main(String[] args) {
         System.out.println("primera imp colab");
+        System.out.println("segunda implem colab");
     }
 }
